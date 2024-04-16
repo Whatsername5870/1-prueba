fde9a9dc2cb3d09b40deb907568ed71bec7f5d50
Drio02 <90213776+Drio02@users.noreply.github.com>
2024-04-15 19:34:08 -0600

    Routes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, request
+from flask import Flask, request, make_response
 from db_postgre import PostgreDatabase
 from db_postgre_service import PostgreDatabaseService
 
@@ -20,3 +20,110 @@ postgre_db_service = PostgreDatabaseService(database=postgre_db)
 @app.route('/')
 def home():
     return 'Hello, World!'
+
+#Endpoints para autenticación
+@app.route('/auth/register', methods = ['POST'])
+def register():
+    None
+
+@app.route('/auth/login', methods = ['POST'])
+def login():
+    None
+
+@app.route('/auth/logout')
+def logout():
+    None
+
+#Endpoints para Usuarios
+@app.route('/users')
+def getUsers():
+    None
+
+@app.route('users/<int:id>')
+def getUser():
+    None
+
+@app.route('/users/<int:id>', methods = ['PUT'])
+def putUser():
+    None
+
+@app.route('/users/<int:id>', methods = ['DELETE'])
+def deleteUser():
+    None
+
+#Enpoints para Encuestas [Anthony]
+@app.route('/surveys', methods = ['POST'])
+def postEncuesta():
+    None
+
+@app.route('/surveys')
+def getSurveys():
+    None
+
+@app.route('/surveys/<int:id>')
+def getSurvey():
+    None
+
+@app.route('/surveys/<int:id>', methods = ['PUT'])
+def putSurvey():
+    None
+
+@app.route('/surveys/<int:id>', methods = ['DELETE'])
+def deleteSurvey():
+    None
+
+@app.route('/surveys/<int:id>/publish', methods = ['POST'])
+def publishSurvey():
+    None
+
+#Endpoints para las Preguntas de las Encuestas [Anthony]
+@app.route('/surveys/<int:id>/questions', methods = ['POST'])
+def postQuestion():
+    None
+
+@app.route('/surveys/<int:id>/questions')
+def getQuestions():
+    None
+
+@app.route('/surveys/<int:id>/questions/<int:questionId>', methods = ['PUT'])
+def putQuestion():
+    None
+
+@app.route('/surveys/<int:id>/questions/<int:questionId>', methods = ['DELETE'])
+def deleteQuestion():
+    None
+
+#Endpoints para las Respuestas de las Encuestas [Anthony]
+@app.route('/surveys/<int:id>/responses', methods = ['POST'])
+def postResponse():
+    None
+
+@app.route('/surveys/<int:id>/responses')
+def getResponses():
+    None
+
+#Endpoints para los encuestados [Decidir - Preguntar algo al profe]
+@app.route('/respondents', methods = ['POST'])
+def postRespondent():
+    None
+
+@app.route('/respondents')
+def getRespondents():
+    None
+
+@app.route('/respondents/<int:id>')
+def getDetailsRespondent():
+    None
+
+@app.route('/respondents/<int:id>', methods = ['PUT'])
+def putDetailsRespondent():
+    None
+
+@app.route('/respondents/<int:id>', methods = ['DELETE'])
+def deleteRespondent():
+    None
+
+#Endpoint para los Reportes y Análisis [Dario - Preguntar acerca del analisis al profe]
+@app.route('/surveys/<int:id>/analysis')
+def getAnalisis():
+    None
